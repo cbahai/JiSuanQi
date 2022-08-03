105218690a245754651dc38ee2b58c535c0f7232
冯勇海 <2206728426@qq.com>
2022-08-03 14:18:01 +0800

    1

--- main.m
+++ main.m
@@ -3,7 +3,7 @@
 //  JiSuanQi
 //
 //  Created by 冯勇海 on 15-3-4.
-//  Copyright (c) 2015年 Chai. All rights reserved.
+//  Copyright (c) 2015年 Chai. All rights reserved...
 //
 
 #import <UIKit/UIKit.h>
